0d6647d91203bcbcef784bb9dc49ad4f6f7abf53
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-23 20:46:17 -0700

    update functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,48 @@ class QuestionsController < ApplicationController
     @question = @survey.questions.new
     render :new
   end
+
+  def create
+    @survey = Survey.find(params[:survey_id])
+    @question = @survey.questions.new(question_params)
+    if @question.save
+      redirect_to survey_path(@survey)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @survey = Survey.find(params[:survey_id])
+    @question = Question.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @survey = Album.find(params[:survey_id])
+    @question = Question.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to survey_path(@question.survey)
+    else
+      @survey = Survey.find(params[:survey_id])
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to question_path(@question.survey)
+  end
+
+
+  private
+    def question_params
+      params.require(:question).permit(:title)
+    end
 end
