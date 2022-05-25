e797b6cc8af702c81c4caed3ed77b77a4be129c9
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-24 20:08:36 -0700

    add code for testing - passes

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,12 @@
 class Survey < ApplicationRecord
   has_many :questions, dependent: :destroy
+  validates :topic, presence: true
+  validates_length_of :topic, maximum: 100
+
+  before_save(:titleize_survey)
+
+  private
+    def titleize_survey
+      self.topic = self.topic.titleize
+    end
 end
