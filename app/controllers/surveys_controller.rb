bf4143c983061380e800f7dd0057c8af65185e87
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-24 20:26:07 -0700

    update method for create in surveys controller.rb

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -13,6 +13,7 @@ class SurveysController < ApplicationController
   def create
     @survey = Survey.new(survey_params)
     if @survey.save
+      flash[:notice] = "Survey successfully added!"
       redirect_to surveys_path
     else
       render :new
