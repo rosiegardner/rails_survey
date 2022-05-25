eef2c14570ed25647a5a16f1f17b4172ab96b2fc
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-24 20:27:33 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_021832) do
+ActiveRecord::Schema.define(version: 2022_05_25_015615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_021832) do
     t.string "topic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
   add_foreign_key "questions", "surveys"
